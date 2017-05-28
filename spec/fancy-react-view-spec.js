f7ddcc758d4e1cf2326b26127e21dc1115d86f7a
Eddie Sholl <eddie.sholl@gmail.com>
2017-05-28 17:15:10 +1000

    init commit of package

---
+++ spec/fancy-react-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FancyReactView from '../lib/fancy-react-view';
+
+describe('FancyReactView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
