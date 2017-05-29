804714b0d82e878b48dd415799493279de05e235
Eddie Sholl <eddie.sholl@gmail.com>
2017-05-29 20:48:15 +1000

    adding some new specs

---
+++ spec/pure/paths-spec.js
@@ -0,0 +1,25 @@
+'use babel'
+
+import paths from '../../lib/pure/paths'
+
+const projectPath1 = '/a/b'
+const projectPath2 = '/c/d'
+
+describe('paths', () => {
+
+  describe('findProjectFor', () => {
+      it('finds a simple case', () => {
+        const result = paths.findProjectFor('/a/b/foo.txt', [projectPath1])
+        expect(result).toEqual(projectPath1)
+      })
+  }),
+
+  describe('sourceFileToTestFile', () => {
+    it('can translate a simple source file', () => {
+      const result = paths.sourceFileToTestFile(
+        projectPath1 + '/client/src/foo/bar.js',
+        projectPath1)
+      expect(result).toEqual(projectPath1 + '/client/test/foo/bar-test.js')
+    })
+  })
+})
