54b419ff9cb454f3ec1ffbd35490c77309278bf1
Eddie Sholl <eddie.sholl@gmail.com>
2017-05-31 22:31:16 +1000

    moved to more testable entry point

---
+++ spec/test-content-spec.js
@@ -0,0 +1,27 @@
+'use babel'
+
+import { generate } from '../lib/test-content'
+
+const sampleModulePath = '/a/b'
+
+const randoFuncInput = `export const randoFunc = (a) => a`
+const randoFuncOutput =
+`
+import { randoFunc } from '${sampleModulePath}'
+
+describe("randoFunc", function () {})`
+
+describe('test-content', () => {
+  describe('generate', () => {
+    it('is empty for invalid cases', () => {
+      const result = generate('', null, sampleModulePath)
+      expect(result).toEqual('')
+    })
+
+    it('handles a single func', () => {
+      const result = generate(
+        randoFuncInput, null, sampleModulePath)
+      expect(result).toEqual(randoFuncOutput)
+    })
+  })
+})
