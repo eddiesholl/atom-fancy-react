47b19de950666bbf13ba3c748fd7e3fb971c6378
Eddie Sholl <eddie.sholl@gmail.com>
2017-06-03 16:06:58 +1000

    basic jsx render func generation

--- spec/test-content-spec.js
+++ spec/test-content-spec.js
@@ -1,6 +1,10 @@
 'use babel'
 
-import { generate } from '../lib/test-content'
+
+import e from 'estree-builder'
+
+import { generate, itBlock, genJs, renderFunc } from '../lib/test-content'
+
 
 const sampleModulePath = '/a/b'
 
@@ -12,16 +16,46 @@ import { randoFunc } from '${sampleModulePath}'
 describe("randoFunc", function () {})`
 
 describe('test-content', () => {
+
   describe('generate', () => {
     it('is empty for invalid cases', () => {
       const result = generate('', null, sampleModulePath)
       expect(result).toEqual('')
     })
 
-    it('handles a single func', () => {
-      const result = generate(
-        randoFuncInput, null, sampleModulePath)
-      expect(result).toEqual(randoFuncOutput)
+  //   it('handles a single func', () => {
+  //     const result = generate(
+  //       randoFuncInput, null, sampleModulePath)
+  //     expect(result).toEqual(randoFuncOutput)
+  //   })
+  })
+
+  describe('renderFunc', () => {
+    it('works', () => {
+      const result = renderFunc('Foo')
+      expect(genJs(result)).toEqual(
+`function renderComponent(props) {
+  props = props || {}
+  return render(<Foo ...props />);
+}`)
+    })
+  })
+
+
+  describe('itBlock', () => {
+    it('works', () => {
+      const result = itBlock('desc', [e.string('body')])
+      const expected =
+`it("desc", function () {
+  "body"
+})`
+      cmp(genJs(result), expected)
     })
   })
 })
+
+const cmp = (ac, exp) => {
+  for (var i = 0; i < ac.length; i++) {
+    expect(ac[i]).toEqual(exp[i])
+  }
+}
