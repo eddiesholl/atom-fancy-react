46b475cedde20f85d5448ad8d684c5d69c922b6b
Eddie Sholl <eddie.sholl@gmail.com>
2017-06-10 11:41:26 +1000

    add import stmts for default and named sut

--- spec/tree-builders-spec.js
+++ spec/tree-builders-spec.js
@@ -74,17 +74,20 @@ describe('tree-builders', () => {
 
   describe('buildImportStmts', () => {
     it('handles empty imports', () => {
-      const result = buildImportStmts([], [])
+      const result = buildImportStmts([])
       expect(genJsList(result)).toEqual('')
     })
 
     it('handles a named import', () => {
-      const result = buildImportStmts([{ 'src/foo': ['named'] }], [])
+    //  const result = buildImportStmts([{ 'src/foo': ['named'] }], [])
+      const input = [{ namedDepImports: { 'src/foo': ['named'] } }]
+      const result = buildImportStmts(input)
       expect(genJsList(result)).toEqual(`import { named } from 'src/foo'`)
     })
 
     it('handles a default import', () => {
-      const result = buildImportStmts([], [{ 'src/foo': 'Default' }])
+      const input = [{ defaultDepImports: { 'src/foo': 'Default' } }]
+      const result = buildImportStmts(input)
       const expected = `import Default from 'src/foo'`
       expect(genJsList(result)).toEqual(expected)
       cmp(genJsList(result), expected)
