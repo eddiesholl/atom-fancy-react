1f3fbe0f5ad1117b6b7d8cc6ced5497d7240b8a5
Eddie Sholl <eddie.sholl@gmail.com>
2017-06-29 22:32:03 +1000

    Add support for new package settings to control paths

--- spec/path-funcs-spec.js
+++ spec/path-funcs-spec.js
@@ -1,32 +1,61 @@
 'use babel'
 
-import paths from '../lib/path-funcs'
-const pathsParallelDirs = paths({
-  testStructure: 'ParallelDirs',
+import pathFuncs from '../lib/path-funcs'
+
+const configBase = {
   packagePath: 'client',
   sourcePath: 'src',
   testPath: 'test',
   testSuffix: '-test',
   projectRoot: '/a/b'
-})
+}
+const configParallelDirs = { testStructure: 'ParallelDirs', ...configBase }
+const configSameDir = { testStructure: 'SameDir', ...configBase }
+const configSubDir = { testStructure: 'SubDir', ...configBase }
+
+const pfParallelDirs = pathFuncs(configParallelDirs)
+const pfSameDir = pathFuncs(configSameDir)
+const pfSubDir = pathFuncs(configSubDir)
 
 const projectPath1 = '/a/b'
 
-describe('paths with ParallelDirs', () => {
+describe('path-funcs with ParallelDirs', () => {
 
   describe('findProjectFor', () => {
       it('finds a simple case', () => {
-        const result = pathsParallelDirs.findProjectFor('/a/b/foo.txt', [projectPath1])
+        const result = pfParallelDirs.findProjectFor('/a/b/foo.txt', [projectPath1])
         expect(result).toEqual(projectPath1)
       })
   }),
 
   describe('sourceFileToTestFile', () => {
     it('can translate a simple source file', () => {
-      const result = pathsParallelDirs.sourceFileToTestFile(
+      const result = pfParallelDirs.sourceFileToTestFile(
         projectPath1 + '/client/src/foo/bar.js',
         projectPath1)
       expect(result).toEqual(projectPath1 + '/client/test/foo/bar-test.js')
+    })
+  })
+})
+
+describe('paths with SameDir', () => {
+  describe('sourceFileToTestFile', () => {
+    it('can translate a simple source file', () => {
+      const result = pfSameDir.sourceFileToTestFile(
+        projectPath1 + '/client/src/foo/bar.js',
+        projectPath1)
+      expect(result).toEqual(projectPath1 + '/client/src/foo/bar-test.js')
+    })
+  })
+})
+
+describe('paths with SubDir', () => {
+  describe('sourceFileToTestFile', () => {
+    it('can translate a simple source file', () => {
+      const result = pfSubDir.sourceFileToTestFile(
+        projectPath1 + '/client/src/foo/bar.js',
+        projectPath1)
+      expect(result).toEqual(projectPath1 + '/client/src/foo/test/bar-test.js')
     })
   })
 })
