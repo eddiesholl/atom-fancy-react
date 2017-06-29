7809bdbfd49857ea8c0c3cb16a50f02e7883d4d2
Eddie Sholl <eddie.sholl@gmail.com>
2017-06-29 22:01:31 +1000

    Refactor path stuff to capture config

--- spec/path-funcs-spec.js
+++ spec/path-funcs-spec.js
@@ -1,21 +1,29 @@
 'use babel'
 
 import paths from '../lib/path-funcs'
+const pathsParallelDirs = paths({
+  testStructure: 'ParallelDirs',
+  packagePath: 'client',
+  sourcePath: 'src',
+  testPath: 'test',
+  testSuffix: '-test',
+  projectRoot: '/a/b'
+})
 
 const projectPath1 = '/a/b'
 
-describe('paths', () => {
+describe('paths with ParallelDirs', () => {
 
   describe('findProjectFor', () => {
       it('finds a simple case', () => {
-        const result = paths.findProjectFor('/a/b/foo.txt', [projectPath1])
+        const result = pathsParallelDirs.findProjectFor('/a/b/foo.txt', [projectPath1])
         expect(result).toEqual(projectPath1)
       })
   }),
 
   describe('sourceFileToTestFile', () => {
     it('can translate a simple source file', () => {
-      const result = paths.sourceFileToTestFile(
+      const result = pathsParallelDirs.sourceFileToTestFile(
         projectPath1 + '/client/src/foo/bar.js',
         projectPath1)
       expect(result).toEqual(projectPath1 + '/client/test/foo/bar-test.js')
