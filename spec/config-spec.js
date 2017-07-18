913d52eb553a77df68355a80acd8964c541c3369
Eddie Sholl <esholl@zendesk.com>
2017-07-18 15:00:54 +1000

    Making config build more robust, handles no package settings

---
+++ spec/config-spec.js
@@ -0,0 +1,23 @@
+'use babel'
+
+import { buildConfig } from '../lib/config'
+
+describe('config', () => {
+
+  describe('buildConfig', () => {
+    const testUserConfig = { testStructure: 'b' }
+    const testPackageConfig = { testStructure: 'c' }
+    const testProjectRoot = '/a/b'
+
+    it('can merge basic config', () => {
+      const result = buildConfig(testUserConfig, testProjectRoot)
+      expect(result.testStructure).toEqual('b')
+      expect(result.projectRoot).toEqual(testProjectRoot)
+    })
+
+    it('can merge package config', () => {
+      const result = buildConfig(testUserConfig, testProjectRoot, testPackageConfig)
+      expect(result.testStructure).toEqual('c')
+    })
+  })
+})
