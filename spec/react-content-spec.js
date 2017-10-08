c4141c2cab6a3c7b3f089354dd9327cca806d9bb
Eddie Sholl <esholl@zendesk.com>
2017-10-08 21:43:05 +1100

    replace hardcoded path with config setting

--- spec/react-content-spec.js
+++ spec/react-content-spec.js
@@ -8,31 +8,31 @@ describe('react-content', () => {
     const importNode1 = {
       type: 'ImportDeclaration',
       loc: { end: { line: 2 }},
-      source: { value: 'src/components/Foo/Foo' }
+      source: { value: 'sourceFolder/components/Foo/Foo' }
     }
     const importNode2 = {
       type: 'ImportDeclaration',
       loc: { end: { line: 6 }},
-      source: { value: 'src/components/Bar/Bar' }
+      source: { value: 'sourceFolder/components/Bar/Bar' }
     }
 
     it('does nothing if exists', () => {
-      const result = importNewComponent([importNode1], 'Foo')
+      const result = importNewComponent([importNode1], 'Foo', 'sourceFolder')
       expect(result).toEqual([])
     })
 
     it('adds an import to no imports', () => {
-      const result = importNewComponent([], 'Foo')
+      const result = importNewComponent([], 'Foo', 'sourceFolder')
       expect(result).toEqual([{
         lineNumber: 0,
-        content: "import Foo from 'src/components/Foo/Foo'"}])
+        content: "import Foo from 'sourceFolder/components/Foo/Foo'"}])
     })
 
     it('adds an import if missing', () => {
-      const result = importNewComponent([importNode2], 'Foo')
+      const result = importNewComponent([importNode2], 'Foo', 'sourceFolder')
       expect(result).toEqual([{
         lineNumber: 7,
-        content: "import Foo from 'src/components/Foo/Foo'"}])
+        content: "import Foo from 'sourceFolder/components/Foo/Foo'"}])
     })
   })
 })
